96bb4e6d733179546b6223919c22f0936b2c56b5
andiyudi <dhieedhiee@gmail.com>
2025-04-25 14:07:50 +0700

    make sh file

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,11 @@
+#Perintah untuk build Docker image dari berkas Dockerfile yang disediakan dengan nama karsajobs:latest (untuk backend).
+echo "Building Docker image for GitHub Package..."
+sudo docker build -t ghcr.io/usernamegithub/karsajobs:latest
+
+#Perintah untuk login ke GitHub Package.
+echo "Logging in to GitHub Package..."
+echo $PAT | docker login ghcr.io -u usernamegithub --password-stdin
+
+#Perintah untuk push image ke GitHub Package.
+echo "Pushing Docker image to GitHub Package..."
+sudo docker push ghcr.io/usernamegithub/karsajobs:latest

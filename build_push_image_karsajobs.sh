0e14b53a943f0610537fa30820281e385c0a3602
andiyudi <dhieedhiee@gmail.com>
2025-04-25 14:33:45 +0700

    change to new token

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -1,10 +1,10 @@
 #Perintah untuk build Docker image dari berkas Dockerfile yang disediakan dengan nama karsajobs:latest (untuk backend).
 echo "Building Docker image for GitHub Package..."
-sudo docker build -t ghcr.io/andiyudi/karsajobs:latest
+sudo docker build -t ghcr.io/andiyudi/karsajobs:latest -f Dockerfile .
 
 #Perintah untuk login ke GitHub Package.
 echo "Logging in to GitHub Package..."
-echo $PAT | docker login ghcr.io -u andiyudi --password-stdin
+echo $PAT2 | docker login ghcr.io -u andiyudi --password-stdin
 
 #Perintah untuk push image ke GitHub Package.
 echo "Pushing Docker image to GitHub Package..."

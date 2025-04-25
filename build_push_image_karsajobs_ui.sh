4a47878b4c18b51294166f3c36139a60dd0f555e
andiyudi <dhieedhiee@gmail.com>
2025-04-25 15:05:11 +0700

    change pat token

--- build_push_image_karsajobs_ui.sh
+++ build_push_image_karsajobs_ui.sh
@@ -4,7 +4,7 @@ sudo docker build -t ghcr.io/andiyudi/karsajobs-ui:latest .
 
 #Perintah untuk login ke GitHub Package.
 echo "Logging in to GitHub Package..."
-echo $PAT | docker login ghcr.io -u andiyudi --password-stdin
+echo $PAT2 | docker login ghcr.io -u andiyudi --password-stdin
 
 #Perintah untuk push image ke GitHub Package.
 echo "Pushing Docker image to GitHub Package..."
